770ca2fc7232638aabfcd614f254028df70f338d
Kris <kris.shcheglova@bk.ru>
2025-01-15 12:55:36 +0200

    add works section , create dynamic gallery

--- app/public/wp-content/themes/photostudio/page-main.php
+++ app/public/wp-content/themes/photostudio/page-main.php
@@ -20,33 +20,26 @@ Template Name: Шаблон: "Главная страница"
 
   <section class="works">
     <div class="container">
-      <h2>Работы</h2>
+      <h2><?= get_field("works_title"); ?></h2>
       <div class="row">
         <div class="col-12">
-          <div class="works-gallery gallery-mobile">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work1.png" alt="work1">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work2.png" alt="work2">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work3.png" alt="work3">
-          </div>
           <div class="works-gallery">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work4.png" alt="work 1">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work5.png" alt="work 2">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work6.png" alt="work 3">
-          </div>
-          <div class="works-gallery">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work7.png" alt="work 4">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work8.png" alt="work 5">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work6.png" alt="work 3">
-          </div>
-          <div class="works-gallery">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work1.png" alt="work1">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work2.png" alt="work2">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work3.png" alt="work3">
-          </div>
-          <div class="works-gallery">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work4.png" alt="work 1">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work5.png" alt="work 2">
-            <img src="<?php echo get_template_directory_uri();?>/assets/img/works/work6.png" alt="work 3">
+             <?php 
+             $counter = 0;
+             if (have_rows('works_photo')) : ?>
+                <?php while (have_rows('works_photo')) : the_row(); ?>
+                    <?php 
+                        $image = get_sub_field('works_photo_item');
+                        if ($image):
+                            $counter++;
+                            if ($counter <= 3 || !wp_is_mobile()) : ?>
+                                <img src="<?php echo esc_url($image['url']); ?>" 
+                                     alt="<?php echo esc_attr($image['alt']); ?>" 
+                                     title="<?php echo esc_attr($image['title']); ?>" />
+                            <?php endif; ?>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
           </div>
         </div>
         <button class="hero-button">Мои работы</button>
@@ -124,7 +117,7 @@ Template Name: Шаблон: "Главная страница"
       </div>
     </div>
   </section>
-
+   
   <section style="text-align: center;">
     <h2>Доступные даты</h2>
     <p>....</p>
